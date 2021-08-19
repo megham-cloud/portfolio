2457eda2e8567fb75688b6c5229e7b8872ac92e1
megham701 <mahapatra.me@ufl.edu>
2021-08-19 14:26:52 -0400

    Added styling to all pages
    - responsive top navigation
    - styling on all pages
    - added new images to folders
    - some responsiveness on home page

--- js/index.js
+++ js/index.js
@@ -15,23 +15,58 @@ $(document).ready(function () {
             var hash = this.hash;
 
             $("html, body").animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top - 96
             }, 800, function () {
                 window.location.hash = hash;
             });
         }
     }); 
 
-    
+});
 
-    });
+// Responsive Top Navigation
+function myFunction() {
 
+    var y = document.getElementById("myLinks");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+}
+
+// Display for myLinks
+var mql = window.matchMedia('(max-width: 768px)');
+
+mql.onchange = function(e) {
+    if (e.matches) {
+        document.getElementById("myLinks").style.display = "none";
+    } else {
+        document.getElementById("myLinks").style.display = "block";
+    }
+}
+
+
+    // let mql = window.matchMedia("(width: 768px)");
+
+    // if (window.innerWidth >= mql.matches) {
+    //     console.log("Wassup??");
+    // }
+
+
+    // var x = document.getElementById("topNav");
+    // if (x.className === "topNav") {
+    //     x.className += ".responsive";
+    // } else {
+    //     x.className = "topNav";
+    // }
     // Display links for top navigation
-    function myFunction() {
-        var x = document.getElementById("myLinks");
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        }
-    }    
+    // function myFunction() {
+    //     var x = document.getElementById("myLinks");
+    //     if (x.style.display === "block") {
+    //         x.style.display = "none";
+    //     } else {
+    //         x.style.display = "block";
+    //     }
+    // }    
